31c3b76ea5341a319ffdc4d7a2c57d73088f3d03
tagirahmad <tagirbpl@gmail.com>
2021-07-08 22:04:55 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CATEGORIES = %w["category 1" "category 2" "category 3" "category 4" "category 5"]
+
+5.times do |index|
+  User.create(name: "User #{index + 1}")
+end
+
+5.times do |index|
+  Category.create(title: "#{CATEGORIES[index]}")
+end
+
+5.times do |index|
+  Test.create(title: "Test #{index + 1}", level: index, category_id: index + 1)
+end
+
+5.times do |index|
+  Question.create(body: "Question body #{index + 1}", test_id: index + 1)
+end
+
+5.times do |index|
+  Answer.create(body: "Answer body #{index + 1}", question_id: index + 1)
+end
