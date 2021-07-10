7f91353b7c0a7088c185642d475365d0b3677d93
tagirahmad <tagirbpl@gmail.com>
2021-07-10 22:14:01 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
   
   def tests_by_level(level)
-    Test.where(level: level, user_id: self.id)
+    Test.joins(:users).where(level: level, users: self.id)
   end
 end
