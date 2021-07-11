f1d86b466e7fd620aba922542a94b2c3962bd1f7
tagirahmad <tagirbpl@gmail.com>
2021-07-11 11:41:41 +0300

    Migration fixes

--- db/migrate/20210710142607_create_join_table_users_tests.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersTests < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :users, :tests do |t|
-      t.index :user_id
-      t.index :test_id
-    end
-  end
-end
