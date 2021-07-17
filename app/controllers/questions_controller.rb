22edd440da929fdf98e703a2903106308dc65b57
Tagir <tagirbpl@gmail.com>
2021-07-17 14:35:35 +0300

    Rubocop fixes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index new create]
   before_action :find_question, only: %i[show destroy edit update]
