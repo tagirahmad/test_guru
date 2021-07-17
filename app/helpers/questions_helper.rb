e26a3b24bcdd6c79dfe2b0698e997df18bb56a83
Tagir <tagirbpl@gmail.com>
2021-07-17 14:38:32 +0300

    Rubocop fixes

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module QuestionsHelper
   def question_header(question)
     "#{question.new_record? ? 'Create' : 'Edit'} #{question.test.title} question"
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module TestsHelper
   def go_back_to_tests
     link_to 'Back', tests_path
