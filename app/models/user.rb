e4135d1e1d399f4beee75f98e8c2f653d1ac3926
Tagir <tagirbpl@gmail.com>
2021-07-18 17:22:42 +0300

    Add initial tests logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :tests_users
-  has_many :tests, through: :tests_users
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
   validates :name, presence: true
 
   def tests_by_level(level)
     Test.joins(:users).where(level: level, users: { id: id })
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
