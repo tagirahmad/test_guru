b4418f09cdb5c2cb9011ca060191027a16ef7e38
Tagir <tagirbpl@gmail.com>
2021-07-27 11:50:36 +0300

    Fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
   has_many :questions, dependent: :destroy
 
