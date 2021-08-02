00bf7d89cd66350f36f0bd527b4f054e1f77e1e8
Tagir <tagirbpl@gmail.com>
2021-08-02 22:27:10 +0300

    Implement js dom to manipulate layouts

---
+++ app/javascript/utilities/passwords_comparing.js
@@ -0,0 +1,24 @@
+document.addEventListener('turbolinks:load', function () {
+    let password_confirmation = document.querySelector('[name = "user[password_confirmation]"]');
+
+    if (password_confirmation) {
+        password_confirmation.addEventListener('input', checkPasswords)
+    }
+})
+
+function checkPasswords() {
+    let password = document.querySelector('[name = "user[password]"]');
+    let password_confirmation = document.querySelector('[name = "user[password_confirmation]"]');
+    let resultField = document.querySelector('.result-comparison')
+
+    if (password.value === 0 || password_confirmation.value === 0) {
+        resultField.classList.remove('hide')
+    }
+
+    if (password.value === password_confirmation.value) {
+        resultField.classList.add('hide')
+    } else {
+        resultField.classList.remove('hide');
+        resultField.style.color = 'red';
+    }
+}
