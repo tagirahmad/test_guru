24169abf511b88a7884920d4b97c4ac8ec24efe6
Tagir <tagirbpl@gmail.com>
2021-08-08 18:30:39 +0300

    Add pg and indexes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_200629) do
+ActiveRecord::Schema.define(version: 2021_08_08_144800) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -66,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_07_31_200629) do
     t.integer "author_id", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
