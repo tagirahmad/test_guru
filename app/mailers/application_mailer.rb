91a8c641095bca98c12a0f5fe158a67967a2bb05
Tagir <tagirbpl@gmail.com>
2021-08-08 21:05:45 +0300

    change application email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: %("TestGuru <mail@testguru.com")
+  default from: 'tagirbpl@gmail.com'
   layout 'mailer'
 end
